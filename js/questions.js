49d7ecc578227a44f426b77868741d67761344d3
Alliiro <86629595+Alliiro@users.noreply.github.com>
2022-07-12 21:03:44 -0300

    .
    #1

---
+++ js/questions.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Qual a principal cor do céu?",
+    answer: "Azul",
+    options: [
+      "Vermelho",
+      "Azul",
+      "Amarelo",
+      "Verde"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Qual o principal motivo pelo qual o céu é Azul?",
+    answer: "Reflexo do mar",
+    options: [
+      "Pq Deus Quis",
+      "Ele não é azul",
+      "Pq sim",
+      "Reflexo do mar"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Qual a menor parte do ser-humano?",
+    answer: "Átomos",
+    options: [
+      "Átomos",
+      "Mindinho",
+      "O Cérebro",
+      "A Memória"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Quantos dedos tem um ser-humano?",
+    answer: "20",
+    options: [
+      "5",
+      "10",
+      "20",
+      "6"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Quantos dedos um ser humano tem nas mãos?",
+    answer: "10",
+    options: [
+      "10",
+      "5",
+      "20",
+      "12"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
