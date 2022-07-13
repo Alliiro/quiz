fd3d1f732595cc35086bff9a46bf32c8caaeef6f
Alliiro <86629595+Alliiro@users.noreply.github.com>
2022-07-13 11:29:32 -0300

    .
    #2

--- js/script.js
+++ js/script.js
@@ -176,7 +176,7 @@ function startTimer(time){
         }
         if(time < 0){ //if timer is less than 0
             clearInterval(counter); //clear counter
-            timeText.textContent = "Time Off"; //change the time text to time off
+            timeText.textContent = "Acabou o tempo"; //change the time text to time off
             const allOptions = option_list.children.length; //getting all option items
             let correcAns = questions[que_count].answer; //getting correct answer from array
             for(i=0; i < allOptions; i++){
@@ -195,11 +195,11 @@ function startTimer(time){
 }
 
 function startTimerLine(time){
-    counterLine = setInterval(timer, 29);
+    counterLine = setInterval(timer, 30);
     function timer(){
         time += 1; //upgrading time value with 1
         time_line.style.width = time + "px"; //increasing width of time_line with px by time value
-        if(time > 549){ //if time value is greater than 549
+        if(time > 1098){ //if time value is greater than 549
             clearInterval(counterLine); //clear counterLine
         }
     }
